a78a6d314e2d3b49a2f122495aa51dbb772bd354
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-05-06 19:12:05 -0300

    Animação completa

--- src/components/Opening.js
+++ src/components/Opening.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Opening = () => {
-    return (
+    const [displayOpening, setDisplayOpening] = useState(true);
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDisplayOpening(false);
+        }, 5000); // 5000 milissegundos = 5 segundos
+        
+        // Limpa o timer ao desmontar o componente para evitar vazamentos de memória
+        return () => clearTimeout(timer);
+    }, []);
 
+    return (
         <>
-        <div className="opening">
-            <img src="./img/rpg-plus-icon-2.png" alt="Logo"></img>
-        </div>
+            <div className={`opening ${displayOpening ? '' : 'hidden'}`}>
+                <img src="./img/rpg-plus-icon-2.png" alt="Logo"></img>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Opening;
